1523cf7afded0b0ed09241ebabbf6dc7d1287a05
Cuerno_de_Mamut <114890320+Andrizi02@users.noreply.github.com>
2026-07-01 23:28:17 -0500

    hola 2

--- src/main/java/com/example/demo4/config/CorsConfig.java
+++ src/main/java/com/example/demo4/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("https://integrador-1-frontend.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
